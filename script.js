99d7eefe255c6a378b4c696337c7a215203a264a
TPauline <73195686+TPauline@users.noreply.github.com>
2022-12-12 05:07:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const wrapper = document.getElementById('wrapper');
 const gameArea = document.getElementById('gameArea');
 uaData = navigator.userAgentData;
+var
+    ua = navigator.userAgent,
+    browser = /Edge\/\d+/.test(ua) ? 'ed' : /MSIE 9/.test(ua) ? 'ie9' : /MSIE 10/.test(ua) ? 'ie10' : /MSIE 11/.test(ua) ? 'ie11' : /MSIE\s\d/.test(ua) ? 'ie?' : /rv\:11/.test(ua) ? 'ie11' : /Firefox\W\d/.test(ua) ? 'ff' : /Chrome\W\d/.test(ua) ? 'gc' : /Chromium\W\d/.test(ua) ? 'oc' : /\bSafari\W\d/.test(ua) ? 'sa' : /\bOpera\W\d/.test(ua) ? 'op' : /\bOPR\W\d/i.test(ua) ? 'op' : typeof MSPointerEvent !== 'undefined' ? 'ie?' : '',
+    os = /Windows NT 10/.test(ua) ? "win10" : /Windows NT 6\.0/.test(ua) ? "winvista" : /Windows NT 6\.1/.test(ua) ? "win7" : /Windows NT 6\.\d/.test(ua) ? "win8" : /Windows NT 5\.1/.test(ua) ? "winxp" : /Windows NT [1-5]\./.test(ua) ? "winnt" : /Mac/.test(ua) ? "mac" : /Linux/.test(ua) ? "linux" : /X11/.test(ua) ? "nix" : "",
+    mobile = /IEMobile|Windows Phone|Lumia/i.test(ua) ? 'w' : /iPhone|iP[oa]d/.test(ua) ? 'i' : /Android/.test(ua) ? 'a' : /BlackBerry|PlayBook|BB10/.test(ua) ? 'b' : /Mobile Safari/.test(ua) ? 's' : /webOS|Mobile|Tablet|Opera Mini|\bCrMo\/|Opera Mobi/i.test(ua) ? 1 : 0,
+    tablet = /Tablet|iPad/i.test(ua),
+    touch = 'ontouchstart' in document.documentElement;
 console.log(navigator.userAgent)
 console.log(navigator.userAgentData)
+console.log("_____|",mobile,tablet,os)
 body = document.body;
 squaresMetrix = []
 
@@ -24,8 +32,9 @@ squaresMetrix = []
 //     return check;
 // };
 
+
 function setup() {
-    if (uaData.mobile) {
+    if (uaData.mobile || !(mobile == 0) || tablet) {
         body.style.width = `${parseInt(screen.width)}px`;
         body.style.height = `${parseInt(screen.height)}px`;
         console.log("mobile/tablet")
